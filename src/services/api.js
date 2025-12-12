5623cba55efdc3621387fb5deb2916619c9da7b1
NattapatJanchaysang <66011360@kmitl.ac.th>
2025-12-13 01:12:00 +0700

    fix http unsecure path to https

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 const API_KEY = "da2f9ee6660fc52a2769e92bf4648283";
-const BASE_URL = "http://api.themoviedb.org/3"
+const BASE_URL = "https://api.themoviedb.org/3"
 
 export const getPopularmovies = async () => {
     const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
@@ -8,7 +8,7 @@ export const getPopularmovies = async () => {
 }
 
 export const searchmovies = async (query) => {
-    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIcomponent(query)}`);
+    const response = await fetch(`${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(query)}`);
     const data = await response.json()
     return data.results
 }
